ad089f919e8a8cc6870fac803b68440112e41007
Daniel Lindemann <daniel@dlindemann.de>
2022-09-26 17:20:03 +0000

    add dev container post create script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,4 +9,4 @@ cd ./src/Web/ClientApp && npm i && cd ../../..
 # install cert with new certifcate path
 # certpath=$(echo $TEMP_ASPNETCORE_Kestrel__Certificates__Default__Path | cut -d'/' -f4-)    # replace /home/<user> with /home/vscode
 # export ASPNETCORE_Kestrel__Certificates__Default__Path="$HOME/$certpath"
-# dotnet dev-certs https --clean --import "$ASPNETCORE_Kestrel__Certificates__Default__Path" --password "$ASPNETCORE_Kestrel__Certificates__Default__Password"
+dotnet dev-certs https --clean --import "$ASPNETCORE_Kestrel__Certificates__Default__Path" --password "$ASPNETCORE_Kestrel__Certificates__Default__Password"

d260097cf14dbb2985e63077682497e49d0432c7
Daniel Lindemann <daniel@dlindemann.de>
2022-09-27 19:47:20 +0000

    upgrade lotto service and randon number service to .net 6

--- src/Web/ClientApp/src/app/lotto-number/lotto-number.component.ts
+++ src/Web/ClientApp/src/app/lotto-number/lotto-number.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LottoField } from '../models/lottoField';
 import { AppConfigService } from '../services/app-config.service';
+import { LottoNumberService } from '../services/lotto-number.service';
 
 @Component({
   selector: 'app-lotto-number-component',
   templateUrl: './lotto-number.component.html',
   styleUrls: ['lotto-number.component.css']
 })
-export class LottoNumberComponent {
+export class LottoNumberComponent implements OnInit {
+  lottoField: LottoField = {} as LottoField;
 
-  constructor() {
+  constructor(private lottoNumberService: LottoNumberService) {
+  }
+
+  ngOnInit(): void {
+    this.loadNumbers();
   }
 
   reload() {
-    debugger;
     // var lottoServiceUrl = this.appConfigService.appConfig.LottoService.url;
+    this.loadNumbers();
+  }
 
+  private loadNumbers() {
+    this.lottoNumberService.getLottoNumber()
+      .subscribe(lottoField => {
+        debugger;
+        this.lottoField = lottoField;
+      });
   }
 }
--- src/Web/ClientApp/src/app/services/lotto-number.service.ts
+++ src/Web/ClientApp/src/app/services/lotto-number.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { LottoField } from '../models/lottoField';
 import { AppConfigService } from './app-config.service';
 
@@ -14,12 +14,36 @@ export class LottoNumberService {
   }
 
   public getLottoNumber() {
-    const url = this.appConfigService.appConfig.lottoService.url + 'api/lottonumber';
-    return this.httpClient.get<LottoField>(url);
+    debugger;
+    const url = this.appConfigService.lottoService.url + '/api/lottonumber';
+    return this.httpClient.get<LottoField>(url)
+      .pipe(
+        tap(_ => this.log('fetched lotto field')),
+        catchError(this.handleError<LottoField>('getLottoNumber', {} as LottoField))
+      );
   }
 
   public getHistory() {
-    // const url = this.appConfigService.appConfig.lottoService.url + 'api/lottonumber/history';
+    // const url = this.appConfigService.lottoService.url + 'api/lottonumber/history';
     // return this.httpClient.get<LottoField>(url);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      debugger;
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log(message);
   }
 }

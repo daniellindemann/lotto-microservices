56814f8ad82c4d8269a69622db722046eef3ad94
Daniel Lindemann <daniel@dlindemann.de>
2022-10-05 10:08:50 +0200

    cleanup certificate load mess

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -25,11 +25,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// if (!app.Environment.IsDevelopment())
+// {
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
 // do not use https - networking assumptions are not the job of the program
 // app.UseHttpsRedirection();

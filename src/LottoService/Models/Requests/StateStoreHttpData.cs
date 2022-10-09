0f3b21ece019c42448dfb973f62117bd8d43aa5d
Daniel Lindemann <daniel@dlindemann.de>
2022-10-09 09:56:33 +0200

    add another json serialization change

--- src/LottoService/Models/Requests/StateStoreHttpData.cs
+++ src/LottoService/Models/Requests/StateStoreHttpData.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace LottoService.Models.Requests;
 
 public class StateStoreHttpData<T> where T : class, new()
 {
+    [JsonPropertyName("key")]
     public string? Key { get; set; }
+
+    [JsonPropertyName("value")]
     public T? Value { get; set; }
 }
